7cb7b113a6864a08de46d6fc993df632bb9a2439
Gleb Golovnev <glebgolovnev200@gmail.com>
2022-10-24 11:26:52 +0300

    Add encrypting and decrypting

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
+import exceptions.CryptoException;
 import org.mariuszgromada.math.mxparser.Expression;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -15,6 +17,16 @@ public class Main {
         try {
             ArchivationFileManager.UnZipFile("file.zip", "file2.txt");
         } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            CryptoUtils.Encrypt("1234567812345678", new File("file2.txt"), new File("file3.txt"));
+        } catch (CryptoException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            CryptoUtils.Decrypt("1234567812345678", new File("file3.txt"), new File("file4.txt"));
+        } catch (CryptoException e) {
             throw new RuntimeException(e);
         }
     }
