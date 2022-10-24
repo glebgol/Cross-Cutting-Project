3f9f5d4b59e43fc526b4d376bcd7326d629387b2
Gleb Golovnev <glebgolovnev200@gmail.com>
2022-10-24 09:25:31 +0300

    Zipped file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,16 @@
 import org.mariuszgromada.math.mxparser.Expression;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Expression expression = new Expression("(3+2) * 2 + 1 + ln(12)");
         System.out.println(expression.calculate());
+        try {
+            ArchivationFileManager.ZipFile("file.txt");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
