1174095383d3ae22829f9d5b9e795d7bf4b58e97
Gleb Golovnev <glebgolovnev200@gmail.com>
2022-10-24 08:40:44 +0300

    Added ArithmeticExpressionParser

--- src/main/java/ArithmeticExpressionParser.java
+++ src/main/java/ArithmeticExpressionParser.java
@@ -1,2 +1,6 @@
 public class ArithmeticExpressionParser {
+    private static final String RegexBrackets = "\\(([1234567890\\.\\+\\-\\*\\/^%]*)\\)";
+    private static final String RegexNum = "[-]?\\d+\\.?\\d*";
+    private static final String RegexMulOp = "[\\*\\/^%]";
+    private static final String RegexAddOp = "[\\+\\-]";
 }
