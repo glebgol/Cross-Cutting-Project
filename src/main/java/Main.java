dddef2e5a09421db9655fdd87713967b9f0c1f70
Gleb Golovnev <glebgolovnev200@gmail.com>
2022-10-24 09:16:13 +0300

    Added another library for counting expressions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,10 @@
-import net.objecthunter.exp4j.Expression;
-import net.objecthunter.exp4j.ExpressionBuilder;
+import org.mariuszgromada.math.mxparser.Expression;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Expression expression = new ExpressionBuilder("(3+2) * 2 + 1 + log(12)").build();
-        var validationResult = expression.validate();
-        if (validationResult.isValid()) {
-            double result = expression.evaluate();
-            System.out.println(result);
-        }
+        Expression expression = new Expression("(3+2) * 2 + 1 + ln(12)");
+        System.out.println(expression.calculate());
     }
 }
