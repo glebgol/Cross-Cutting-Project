b87545fc4e110620b63e55f93fd484508021bc24
Gleb Golovnev <glebgolovnev200@gmail.com>
2022-11-06 18:44:02 +0300

    Deleted DefaultReader

--- src/main/java/readers/TxtFileReader.java
+++ src/main/java/readers/TxtFileReader.java
@@ -3,22 +3,22 @@ package readers;
 import exceptions.CryptoException;
 import interfaces.IFileReader;
 import interfaces.IStream;
+import parsers.ProcessingTxtFile;
+import streams.CalculationResult;
+import streams.ReadingResult;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 
 public class TxtFileReader implements IFileReader {
 
-    protected String inputFilename;
-    protected String outputFilename;
-    protected IFileReader _reader;
+    private String inputFilename;
+    private String outputFilename;
 
-    public TxtFileReader(IFileReader reader) {
-        _reader = reader;
-        inputFilename = reader.getInputFilename();
-        outputFilename = reader.getOutputFilename();
+    public TxtFileReader(String inputFilename, String outputFilename) {
+        this.inputFilename = inputFilename;
+        this.outputFilename = outputFilename;
     }
-
     @Override
     public String getInputFilename() {
         return inputFilename;
@@ -30,27 +30,39 @@ public class TxtFileReader implements IFileReader {
     }
 
     @Override
-    public void Write(IStream stream) throws IOException, CryptoException {
-        _reader.Write(stream);
+    public void Write(IStream stream) throws IOException {
+        FileOutputStream outputFile = new FileOutputStream(outputFilename);
+        outputFile.write(stream.bytes());
+        outputFile.close();
     }
 
     @Override
-    public IStream Read() throws IOException, CryptoException {
-        return _reader.Read();
+    public IStream Read() throws IOException {
+        File inputFile = new File(inputFilename);
+        FileInputStream fileInputStream = new FileInputStream(inputFile);
+        var bytes = fileInputStream.readAllBytes();
+        fileInputStream.close();
+        return new ReadingResult(bytes);
     }
 
     @Override
-    public IStream Transform(IStream stream) throws IOException, CryptoException {
-        return _reader.Transform(stream);
+    public IStream Transform(IStream stream) {
+        return stream;
     }
 
     @Override
-    public IStream Calculate(IStream stream) throws IOException, CryptoException {
-        return _reader.Calculate(stream);
+    public IStream Calculate(IStream stream) {
+        var calculatedLines = new ArrayList<String>();
+        for (var line : stream.lines()) {
+            var calculateLine = ProcessingTxtFile.CalculateLine(line);
+            calculatedLines.add(calculateLine);
+        }
+        var calculationResult = new CalculationResult(calculatedLines);
+        return calculationResult;
     }
 
     @Override
-    public void WriteCalculated() throws IOException, CryptoException {
+    public void WriteCalculated() throws IOException {
         var readingResult = Read();
         var calculatedResult = Calculate(readingResult);
         Write(calculatedResult);
