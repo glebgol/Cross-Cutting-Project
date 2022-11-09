c8b0c042c1d79ef9a50b7c87c0237da0d571d045
Gleb Golovnev <glebgolovnev2004@gmail.com>
2022-11-10 01:27:06 +0300

    Created stub methods

--- rest-api/src/main/java/com/example/restapi/FileReaderController.java
+++ rest-api/src/main/java/com/example/restapi/FileReaderController.java
@@ -1,17 +1,38 @@
 package com.example.restapi;
 
+import org.springframework.http.HttpCookie;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
+import java.net.http.HttpResponse;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/calculate/")
+@RequestMapping("api/")
 public class FileReaderController {
-    @GetMapping
-    public String Add(@RequestParam(required = false) boolean zip, @RequestParam(value="encrypt", required = false) List<String> decryptionKeys) {
-        return String.valueOf(zip) + decryptionKeys;
+    @GetMapping("calculate/")
+    public ResponseEntity<String> Calculate(@RequestParam(value= "inputfile") String inputFilename,
+                            @RequestParam(value = "outputfile") String outputFilename,
+                            @RequestParam(required = false) boolean zip,
+                            @RequestParam(value="decryptionkeys", required = false) List<String> decryptionKeys) {
+
+        return new ResponseEntity<>(inputFilename + " " + outputFilename + " " + String.valueOf(zip) + " " + decryptionKeys, HttpStatus.OK);
+    }
+
+    @GetMapping("encrypt/")
+    public ResponseEntity<String> Encrypt(@RequestParam(value= "inputfile") String inputFilename,
+                                            @RequestParam(value = "outputfile") String outputFilename,
+                                            @RequestParam(value="key", required = false) String key) {
+
+        return new ResponseEntity<>(inputFilename + " " + outputFilename + " " + key, HttpStatus.OK);
+    }
+
+    @GetMapping("zip/")
+    public ResponseEntity<String> Zip(@RequestParam(value= "inputfile") String inputFilename,
+                                          @RequestParam(value = "outputfile") String outputFilename) {
+
+        return new ResponseEntity<>(inputFilename + " " + outputFilename, HttpStatus.OK);
     }
 }
