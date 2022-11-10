cb45614e0cba47b34946d1dd0a52517f5cc83494
Gleb Golovnev <glebgolovnev2004@gmail.com>
2022-11-10 20:39:56 +0300

    Added setDecryption in Calculate method

--- rest-api/src/main/java/com/example/restapi/FileReaderController.java
+++ rest-api/src/main/java/com/example/restapi/FileReaderController.java
@@ -12,9 +12,12 @@ import java.net.http.HttpResponse;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/")
+@RequestMapping("api/file-reader/")
 public class FileReaderController {
     private boolean isValidDecryptionKeys(List<String> decryptionKeys) {
+        if (decryptionKeys == null) {
+            return true;
+        }
         for (var key : decryptionKeys) {
             if (key.length() != 16) {
                 return false;
@@ -23,30 +26,34 @@ public class FileReaderController {
         return true;
     }
 
+    // http://localhost:8080/api/file-reader/calculate/?inputfile=double_encrypted.zip&outputfile=qwe.txt&iszipped=true&decryptionkeys=qwsdcvbgfthyrdfw,asdfghjkqewrtyto
     @GetMapping("calculate/")
     public ResponseEntity<String> Calculate(@RequestParam(value= "inputfile") String inputFilename,
                             @RequestParam(value = "outputfile") String outputFilename,
-                            @RequestParam(required = false) boolean zip,
+                            @RequestParam(value = "iszipped", required = false) boolean isZipped,
                             @RequestParam(value="decryptionkeys", required = false) List<String> decryptionKeys) {
 
-        if (decryptionKeys != null && !isValidDecryptionKeys(decryptionKeys)) {
+        if (!isValidDecryptionKeys(decryptionKeys)) {
             return new ResponseEntity<>("Key must be 16 length, when decrypting with padded cipher", HttpStatus.BAD_REQUEST);
         }
 
         try {
             var readerBuilder = new FileReaderBuilder(inputFilename, outputFilename);
             readerBuilder.setFileExtension(FileExtension.Txt);
-            if (zip) {
+            if (decryptionKeys != null) {
+                for (var key : decryptionKeys) {
+                    readerBuilder.setEncrypting(key);
+                }
+            }
+            if (isZipped) {
                 readerBuilder.setZipping();
             }
             var reader = readerBuilder.getResult();
             reader.WriteCalculated();
-
         } catch (Exception ex) {
-            return new ResponseEntity<>("Error", HttpStatus.BAD_REQUEST);
-
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<>(inputFilename + " " + outputFilename + " " + String.valueOf(zip) + " " + decryptionKeys, HttpStatus.OK);
+        return new ResponseEntity<>(inputFilename + " " + outputFilename + " " + isZipped + " " + decryptionKeys, HttpStatus.OK);
     }
 
     @GetMapping("encrypt/")
