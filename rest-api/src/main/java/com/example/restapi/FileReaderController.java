2ba3187b5dff25e27cda62dd28068c2eb4c88842
Gleb Golovnev <glebgolovnev2004@gmail.com>
2022-11-10 19:13:38 +0300

    Added HttpGet Calculate method

--- rest-api/src/main/java/com/example/restapi/FileReaderController.java
+++ rest-api/src/main/java/com/example/restapi/FileReaderController.java
@@ -1,5 +1,7 @@
 package com.example.restapi;
 
+import builder.FileReaderBuilder;
+import enums.FileExtension;
 import org.springframework.http.HttpCookie;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,12 +14,38 @@ import java.util.List;
 @RestController
 @RequestMapping("api/")
 public class FileReaderController {
+    private boolean isValidDecryptionKeys(List<String> decryptionKeys) {
+        for (var key : decryptionKeys) {
+            if (key.length() != 16) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     @GetMapping("calculate/")
     public ResponseEntity<String> Calculate(@RequestParam(value= "inputfile") String inputFilename,
                             @RequestParam(value = "outputfile") String outputFilename,
                             @RequestParam(required = false) boolean zip,
                             @RequestParam(value="decryptionkeys", required = false) List<String> decryptionKeys) {
 
+        if (decryptionKeys != null && !isValidDecryptionKeys(decryptionKeys)) {
+            return new ResponseEntity<>("Key must be 16 length, when decrypting with padded cipher", HttpStatus.BAD_REQUEST);
+        }
+
+        try {
+            var readerBuilder = new FileReaderBuilder(inputFilename, outputFilename);
+            readerBuilder.setFileExtension(FileExtension.Txt);
+            if (zip) {
+                readerBuilder.setZipping();
+            }
+            var reader = readerBuilder.getResult();
+            reader.WriteCalculated();
+
+        } catch (Exception ex) {
+            return new ResponseEntity<>("Error", HttpStatus.BAD_REQUEST);
+
+        }
         return new ResponseEntity<>(inputFilename + " " + outputFilename + " " + String.valueOf(zip) + " " + decryptionKeys, HttpStatus.OK);
     }
 
