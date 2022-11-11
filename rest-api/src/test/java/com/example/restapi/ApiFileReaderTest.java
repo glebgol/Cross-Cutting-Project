9d53a9be716a27e21a20e6ba1da590c4cc600442
Gleb Golovnev <glebgolovnev2004@gmail.com>
2022-11-11 16:04:32 +0300

    Api tests for calculate

--- rest-api/src/test/java/com/example/restapi/ApiFileReaderTest.java
+++ rest-api/src/test/java/com/example/restapi/ApiFileReaderTest.java
@@ -5,18 +5,32 @@ import org.junit.jupiter.api.Test;
 
 
 class ApiFileReaderTest {
+    private final String Uri = "http://localhost:8080/api/file-reader";
+    private final String NonExistingFilename = "NonExistingFilename";
+    private final String ExistingFilename = "rest-api/src/test/resources/double_encrypted.zip";
+    private final String OutputFilename = "rest-api/src/test/resources/output-file.txt";
+    private final String DecryptionKeys = "qwsdcvbgfthyrdfw,asdfghjkqewrtyto";
 
     @Test
     void Calculate_Returns_200_StatusCode() {
+        var uri = String.format("%s/calculate/?inputfile=%s&outputfile=%s&iszipped=true&decryptionkeys=%s", Uri, ExistingFilename, OutputFilename, DecryptionKeys);
+
         RestAssured
                 .when()
-                .get("http://localhost:8080/api/file-reader/calculate/?inputfile=D://Anton//Cross-Cutting-Project//double_encrypted.zip&outputfile=output2.txt&iszipped=true&decryptionkeys=qwsdcvbgfthyrdfw,asdfghjkqewrtyto")
+                .get(uri)
                 .then()
                 .statusCode(200);
     }
 
     @Test
     void Calculate_Returns_400_StatusCode() {
+        var uri = String.format("%s/calculate/?inputfile=%s&outputfile=%s&iszipped=true&decryptionkeys=%s", Uri, NonExistingFilename, OutputFilename, DecryptionKeys);
+
+        RestAssured
+                .when()
+                .get(uri)
+                .then()
+                .statusCode(400);
     }
 
     @Test
