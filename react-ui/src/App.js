97842a415bc88d799dd9f1de2dfc63661e572d58
Gleb Golovnev <glebgolovnev2004@gmail.com>
2022-11-11 19:16:38 +0300

    Changed App

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>File Reader</h1>
+        <div>
+            <div>
+                <h3>Enter file name:</h3>
+                <input/>
+            </div>
+            <div>
+                <button>
+                    Zip
+                </button>
+                <button>
+                    Calculate
+                </button>
+                <button>
+                    Encrypt
+                </button>
+            </div>
+        </div>
     </div>
   );
 }
---
+++ react-ui/src/Input.js
@@ -0,0 +1,14 @@
+
+function Input() {
+    return (
+        <div>
+            <h2>File name</h2>
+            <div>
+                <input/>
+                <button>ok</button>
+            </div>
+        </div>
+    )
+}
+
+export default Input;
