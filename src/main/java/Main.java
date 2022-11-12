0c9611ca0f6df2c6d59a369cd5514e3cfc0d3193
Gleb Golovnev <glebgolovnev2004@gmail.com>
2022-11-12 10:15:43 +0300

    Edit test run configurations

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) throws IOException, CryptoException {
         String FirstKey = "qwsdcvbgfthyrdfw";
         String SecondKey = "asdfghjkqewrtyto";
-        var builder = new FileReaderBuilder("src/test/resources/double_encrypted.zip", "output.txt");
+        var builder = new FileReaderBuilder("double_encrypted.zip", "output.txt");
         builder.setFileExtension(FileExtension.Txt);
 
         builder.setEncrypting(FirstKey);
