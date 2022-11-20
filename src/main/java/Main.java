7666203ac04d309cbd28b1a9c896b902bf72b4f2
Gleb Golovnev <glebgolovnev2004@gmail.com>
2022-11-20 16:48:49 +0300

    Calculated xml, json and double encrypted files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,38 +1,29 @@
-import ciphers.CryptoUtils;
+import builder.FileReaderBuilder;
+import enums.FileExtension;
 import exceptions.CryptoException;
-import parsers.json.JsonExpression;
-import parsers.json.JsonExpressionList;
-import parsers.xml.XmlExpression;
-import parsers.xml.XmlExpressionList;
+import readers.JsonFileReader;
 import readers.XmlFileReader;
 
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
-import java.util.List;
 
 public class Main {
     private static final String FirstKey = "qwsdcvbgfthyrdfw";
     private static final String SecondKey = "asdfghjkqewrtyto";
     public static void main(String[] args) throws IOException, CryptoException, JAXBException {
-//        String FirstKey = "qwsdcvbgfthyrdfw";
-//        String SecondKey = "asdfghjkqewrtyto";
-//        var builder = new FileReaderBuilder("double_encrypted.zip", "output.txt");
-//        builder.setFileExtension(FileExtension.Txt);
-//
-//        builder.setEncrypting(FirstKey);
-//        builder.setEncrypting(SecondKey);
-//
-//        builder.setZipping();
-//
-//        var reader = builder.getResult();
-//
-//        reader.WriteCalculated();
-//        var reader = new JsonFileReader("input.json", "output.json");
-//        var expList = new ExpressionList(new ArrayList<>(List.of(new ExpressionObject("12-100", "12-345", "12"))));
-//        reader.WriteJsonExpressions(expList);
 
+        var builder = new FileReaderBuilder(FileExtension.Txt, "double_encrypted.zip", "output.txt");
+        builder.setEncrypting(FirstKey);
+        builder.setEncrypting(SecondKey);
+        builder.setZipping(true);
 
-        CryptoUtils.Encrypt("qwsdcvbgfthyrdfw", "src/test/resources/input.json", "src/test/resources/enc_json.txt");
+        var reader = builder.getResult();
+        reader.WriteCalculated();
 
+        var jsonReader = new JsonFileReader("input.json", "output.json");
+        jsonReader.WriteCalculated();
+
+        var xmlReader = new XmlFileReader("input.xml", "output.xml");
+        xmlReader.WriteCalculated();
     }
 }
