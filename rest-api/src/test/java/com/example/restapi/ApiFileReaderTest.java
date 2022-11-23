a7fd3aa1c4f1695d641899275824f8f7b3c9e0da
Gleb Golovnev <glebgolovnev2004@gmail.com>
2022-11-23 21:44:46 +0300

    Created api test for not valid file extension

--- rest-api/src/test/java/com/example/restapi/ApiFileReaderTest.java
+++ rest-api/src/test/java/com/example/restapi/ApiFileReaderTest.java
@@ -13,6 +13,7 @@ class ApiFileReaderTest {
     protected final String DecryptionKeys = "qwsdcvbgfthyrdfw,asdfghjkqewrtyto";
     protected final String DecryptionKey = "qwsdcvbgfthyrdfw";
     protected final String FileExtension = "txt";
+    protected final String NotValidExtension = "yaml";
     protected final String NotValidDecryptionKeys = "abra,cada,bra";
     @Test
     void Calculate_ExistingFile_Returns_200_StatusCode() {
@@ -39,6 +40,17 @@ class ApiFileReaderTest {
     @Test
     void Calculate_NotValidKeys_Returns_400_StatusCode() {
         var uri = String.format("%s/calculate/?inputfile=%s&outputfile=%s&iszipped=true&decryptionkeys=%s&extension=%s", Uri, ExistingFilename, OutputFilename, NotValidDecryptionKeys, FileExtension);
+
+        RestAssured
+                .when()
+                .get(uri)
+                .then()
+                .statusCode(400);
+    }
+
+    @Test
+    void Calculate_NotValidExtension_Returns_400_StatusCode() {
+        var uri = String.format("%s/calculate/?inputfile=%s&outputfile=%s&iszipped=true&decryptionkeys=%s&extension=%s", Uri, ExistingFilename, OutputFilename, DecryptionKeys, NotValidExtension);
 
         RestAssured
                 .when()
