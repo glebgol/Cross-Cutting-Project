7c7e446625cbf6b22195b2e1ea2d82a72b860103
Gleb Golovnev <glebgolovnev2004@gmail.com>
2022-12-11 17:59:10 +0300

    Create routing

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
-import Input from "./Input";
+import Calculate from "./components/Calculate";
+import Decrypt from "./components/Decrypt";
+import UnZip from "./components/UnZip";
+import Zip from "./components/Zip";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Encrypt from "./components/Encrypt";
 
 function App() {
   return (
-    <div className="App">
-      <h1>File Reader</h1>
-        <Input></Input>
-    </div>
+      <BrowserRouter>
+        <div className="App">
+            <Navbar />
+            <div>
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route path="calculate/" element={<Calculate />} />
+                    <Route path="decrypt/" element={<Decrypt />} />
+                    <Route path="encrypt/" element={<Encrypt />} />
+                    <Route path="zip/" element={<Zip />} />
+                    <Route path="unzip/" element={<UnZip />} />
+                </Routes>
+            </div>
+        </div>
+      </BrowserRouter>
   );
 }
 
