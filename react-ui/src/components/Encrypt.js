6b57078ce11ee3b0ce581e2642da8236e75b3b10
Gleb Golovnev <glebgolovnev2004@gmail.com>
2022-12-11 22:01:10 +0300

    Fetch Encrypting

--- react-ui/src/components/Encrypt.js
+++ react-ui/src/components/Encrypt.js
@@ -1,8 +1,46 @@
+import {useState} from "react";
+import {Link} from "react-router-dom";
+import fetchEncrypting from "../fetching/fetchEncrypting";
+
 const Encrypt = () => {
+    const [inputFile, setInputFile] = useState('')
+    const [outputFile, setOutputFile] = useState('')
+    const [key, setKey] = useState('')
+    const [resultInfo, setResultInfo] = useState('')
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetchEncrypting(inputFile, outputFile, key, setResultInfo);
+    }
     return (
-        <h2>
-            Encrypt
-        </h2>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>Input file:</label>
+                <input
+                    type="text"
+                    required
+                    value={inputFile}
+                    onChange={(e) => setInputFile(e.target.value)}
+                />
+                <label>Output file:</label>
+                <input
+                    type="text"
+                    required
+                    value={outputFile}
+                    onChange={(e) => setOutputFile(e.target.value)}
+                />
+                <label>Encrypted keys</label>
+                <input
+                    type="text"
+                    value={key}
+                    onChange={(e) => {
+                        setKey(e.target.value);
+                    }}
+                />
+                <button>Encrypt</button>
+            </form>
+            <p><b>{resultInfo}</b></p>
+            <Link to="/">Back to Home Page</Link>
+        </div>
     );
 }
 
---
+++ react-ui/src/fetching/fetchEncrypting.js
@@ -0,0 +1,10 @@
+const fetchCalculating = (inputFile, outputFile, key, setState) => {
+    const url = 'api/file-reader/encrypt/?inputfile=' + inputFile + '&outputfile=' + outputFile + '&key=' + key;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            setState(data.resultInfo);
+        });
+}
+
+export default fetchCalculating;
