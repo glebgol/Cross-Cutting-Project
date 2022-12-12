8631a48bf2d2555f9380fd85aaaa4fe8969b8feb
Gleb Golovnev <glebgolovnev2004@gmail.com>
2022-12-13 01:15:09 +0300

    Form validation in process

--- reactui/src/components/Calculate.js
+++ reactui/src/components/Calculate.js
@@ -4,11 +4,40 @@ import fetchCalculating from "../fetching/fetchCalculating";
 
 const Calculate = () => {
     const [inputFile, setInputFile] = useState('')
+    const [inputFileIsDirty, setInputFileIsDirty] = useState(false)
+    const [inputFileError, setInputFileError] = useState('Input file name can\'t be empty')
+
     const [outputFile, setOutputFile] = useState('')
+    const [outputFileIsDirty, setOutputFileIsDirty] = useState(false)
+    const [outputFileError, setOutputFileError] = useState('Output file name can\'t be empty')
+
     const [isZipped, setIsZipped] = useState(false)
     const [keys, setKeys] = useState('')
+
     const [extension, setExtension] = useState('Txt')
     const [resultInfo, setResultInfo] = useState('')
+
+    const inputFileBlurHandler = (e) => {
+        setInputFileIsDirty(true)
+    }
+    const outputFileBlurHandler = (e) => {
+        setOutputFileIsDirty(true)
+    }
+
+    const inputFileOnChangeHandler = (e) => {
+        setInputFile(e.target.value)
+
+        //todo validation
+        const allowedExtensions =
+            '.txt';
+
+        if (e.target.value !== '' && !allowedExtensions.exec(e.target.value)) {
+            setInputFileError('Not valid file name')
+        } else {
+            setInputFile(e.target.value)
+            setInputFileError('')
+        }
+    }
     const handleSubmit = (e) => {
         e.preventDefault();
         fetchCalculating(inputFile, outputFile, isZipped, keys, extension, setResultInfo);
@@ -17,17 +46,25 @@ const Calculate = () => {
         <div>
             <form onSubmit={handleSubmit}>
                 <label>Input file:</label>
+                {(inputFileIsDirty && inputFileError) && <div style={{color:'red'}}>{inputFileError}</div>}
                 <input
+                    placeholder='Enter input file name:'
+                    name='inputFile'
                     type="text"
                     required
                     value={inputFile}
-                    onChange={(e) => setInputFile(e.target.value)}
+                    onBlur={e => inputFileBlurHandler(e)}
+                    onChange={(e) => inputFileOnChangeHandler(e)}
                 />
                 <label>Output file:</label>
+                {(outputFileIsDirty && outputFileError) && <div style={{color:'red'}}>{outputFileError}</div>}
                 <input
+                    placeholder='Enter output file name:'
+                    name='outputFile'
                     type="text"
                     required
                     value={outputFile}
+                    onBlur={e => outputFileBlurHandler(e)}
                     onChange={(e) => setOutputFile(e.target.value)}
                 />
                 <label>Is zipped?</label>
