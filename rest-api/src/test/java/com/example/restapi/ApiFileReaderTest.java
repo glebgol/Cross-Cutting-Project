7a00c0616837f75eb34220baa3dcb9464f8578c1
Gleb Golovnev <glebgolovnev2004@gmail.com>
2022-12-22 13:43:13 +0300

    Added query params to rest-assured tests

--- rest-api/src/test/java/com/example/restapi/ApiFileReaderTest.java
+++ rest-api/src/test/java/com/example/restapi/ApiFileReaderTest.java
@@ -1,30 +1,38 @@
 package com.example.restapi;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 
 
 class ApiFileReaderTest {
     protected final String Uri = "http://localhost:8080/api/file-reader";
+    protected final String CalculateUri = Uri + "/calculate/";
     protected final String NonExistingFilename = "NonExistingFilename";
     protected final String ExistingFilename = "rest-api/src/test/resources/double_encrypted.zip";
-    protected final String InputFilename = "rest-api/src/test/resources/input-file.txt";
-    protected final String OutputFilename = "rest-api/src/test/resources/output-file.txt";
+    protected final String InputFilename = "rest-api//src//test//resources//input-file.txt";
+    protected final String OutputFilename = "rest-api//src//test//resources//output-file.txt";
     protected final String DecryptionKeys = "qwsdcvbgfthyrdfw,asdfghjkqewrtyto";
+    protected final Object[] DecryptionKeysList = new String[] {"qwsdcvbgfthyrdfw", "asdfghjkqewrtyto" };
     protected final String DecryptionKey = "qwsdcvbgfthyrdfw";
     protected final String FileExtension = "txt";
     protected final String NotValidExtension = "yaml";
     protected final String NotValidDecryptionKeys = "abra,cada,bra";
+    protected final boolean IsZipped = true;
+
     @Test
     void Calculate_ExistingFile_Returns_200_StatusCode() {
-        var uri = String.format("%s/calculate/?inputfile=%s&outputfile=%s&iszipped=true&decryptionkeys=%s&extension=%s", Uri, ExistingFilename, OutputFilename, DecryptionKeys, FileExtension);
-
-
-        when()
-                .get(uri)
-                .then()
-                .statusCode(200);
+        given().contentType(ContentType.JSON)
+                .queryParam("inputfile", ExistingFilename)
+                .queryParam("outputfile", OutputFilename)
+                .queryParam("iszipped", IsZipped)
+                .queryParam("decryptionkeys", DecryptionKeysList)
+                .queryParam("extension", FileExtension)
+                .log().parameters()
+                .log().uri()
+                .when().get(CalculateUri).then().log().all().statusCode(200);
     }
 
     @Test
