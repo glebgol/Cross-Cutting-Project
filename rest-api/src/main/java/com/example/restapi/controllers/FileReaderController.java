162edb2ca1b9b8815fc680c8d102ab3500b0f6f6
Gleb Golovnev <glebgolovnev2004@gmail.com>
2022-12-23 18:17:24 +0300

    File-upload-download util

--- rest-api/src/main/java/com/example/restapi/controllers/FileReaderController.java
+++ rest-api/src/main/java/com/example/restapi/controllers/FileReaderController.java
@@ -5,14 +5,36 @@ import builder.FileReaderBuilder;
 import ciphers.CryptoUtils;
 import ciphers.KeyValidation;
 import com.example.restapi.responses.*;
+import com.example.restapi.utils.FileUploadUtil;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
 @RequestMapping("api/file-reader/")
 public class FileReaderController {
+    @PostMapping("/uploadFile")
+    public ResponseEntity<FileUploadResponse> uploadFile(
+            @RequestParam("file") MultipartFile multipartFile)
+            throws IOException {
+
+        String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+        long size = multipartFile.getSize();
+
+        String filecode = FileUploadUtil.saveFile(fileName, multipartFile);
+
+        FileUploadResponse response = new FileUploadResponse();
+        response.setFileName(fileName);
+        response.setSize(size);
+        response.setDownloadUri("/downloadFile/" + filecode);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
     @GetMapping("calculate/")
     public ResponseEntity<CalculateResponse> calculate(@RequestParam(value= "inputfile") String inputFilename,
                             @RequestParam(value = "outputfile") String outputFilename,
