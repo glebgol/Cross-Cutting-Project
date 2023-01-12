5792cc3f65bf9bdd5d83ac2faee5736233c920d9
Gleb Golovnev <glebgolovnev2004@gmail.com>
2023-01-12 17:57:09 +0300

    Create EncryptionPage and EncryptionTest

---
+++ selenium-ui-tests/src/test/java/reactuitest/EncryptionPage.java
@@ -0,0 +1,60 @@
+package reactuitest;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class EncryptionPage extends BaseSeleniumPage {
+    private final String URL = "http://localhost:3000/encrypt";
+
+    @FindBy(xpath = "//input[@type='file']")
+    private WebElement fileInput;
+
+    @FindBy(xpath = "//input[@name='outputFile']")
+    private WebElement outputFileNameInput;
+
+    @FindBy(xpath = "//input[@name='key']")
+    private WebElement keyInput;
+
+    @FindBy(xpath = "//button[@name='encrypt']")
+    private WebElement encryptButton;
+
+    @FindBy(xpath = "//button[@name=\"download\"]")
+    private WebElement downloadButton;
+
+    public EncryptionPage() {
+        driver.get(URL);
+        PageFactory.initElements(driver, this);
+    }
+
+    public EncryptionPage setFile(String filePath) {
+        fileInput.sendKeys(filePath);
+        return this;
+    }
+
+    public EncryptionPage setOutputFileName(String fileName) {
+        outputFileNameInput.sendKeys(fileName);
+        return this;
+    }
+
+    public EncryptionPage setEncryptedKey(String key) {
+        keyInput.sendKeys(key);
+        return this;
+    }
+
+    public void encrypt() {
+        encryptButton.click();
+    }
+
+    public void download() {
+        downloadButton.click();
+    }
+
+    public boolean isEnabledEncryptButton() {
+        return encryptButton.isEnabled();
+    }
+
+    public boolean isEnabledDownloadButton() {
+        return downloadButton.isEnabled();
+    }
+}
