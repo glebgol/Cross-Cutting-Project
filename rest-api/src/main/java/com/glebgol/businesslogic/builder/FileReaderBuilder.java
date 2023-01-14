62a1baf5d3a3d88a531da898411630ae19c9dab8
Gleb Golovnev <glebgolovnev2004@gmail.com>
2023-01-14 14:54:37 +0300

    Add to builder throw IllegalArgumentException for not supported file extension

--- rest-api/src/main/java/com/glebgol/businesslogic/builder/FileReaderBuilder.java
+++ rest-api/src/main/java/com/glebgol/businesslogic/builder/FileReaderBuilder.java
@@ -11,29 +11,19 @@ import java.util.List;
 public class FileReaderBuilder implements IFileReaderBuilder {
     protected IFileReader fileReader;
     protected String inputFilename;
-    public FileReaderBuilder(FileExtension extension, String inputFilename) {
-        if (extension == FileExtension.Txt) {
-            fileReader = new TxtFileReader(inputFilename);
-        }
-        else if (extension == FileExtension.Json) {
-            fileReader = new JsonFileReader(inputFilename);
-        }
-        else if (extension == FileExtension.Xml) {
-            fileReader = new XmlFileReader(inputFilename);
-        }
-        this.inputFilename = inputFilename;
-    }
-
     public FileReaderBuilder(String extension, String inputFilename) {
-        var fileExtension = extension.toUpperCase();
+        String fileExtension = extension.toUpperCase();
         if (fileExtension.equals(FileExtension.Txt.name().toUpperCase())) {
             fileReader = new TxtFileReader(inputFilename);
         }
         else if ((fileExtension.equals(FileExtension.Json.name().toUpperCase()))){
             fileReader = new JsonFileReader(inputFilename);
         }
-        else {
+        else if ((fileExtension.equals(FileExtension.Xml.name().toUpperCase()))) {
             fileReader = new XmlFileReader(inputFilename);
+        }
+        else {
+            throw new IllegalArgumentException("Not supported file extension :\"" + extension + "\"");
         }
         this.inputFilename = inputFilename;
     }
