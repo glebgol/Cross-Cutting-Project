034e538c6b3cc6fcaeafb12f638c1b4ffee839fa
Gleb Golovnev <glebgolovnev2004@gmail.com>
2023-01-23 14:51:38 +0300

    Create Spring Boot tests for encrypting

--- rest-api/src/test/java/com/glebgol/restapi/controllers/FileEncryptionControllerTest.java
+++ rest-api/src/test/java/com/glebgol/restapi/controllers/FileEncryptionControllerTest.java
@@ -1,46 +1,43 @@
 package com.glebgol.restapi.controllers;
 
-import com.glebgol.restapi.Urls.Urls;
-import com.glebgol.testvalues.TestValues;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.io.File;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import static com.glebgol.testvalues.TestValues.MESSAGE_FOR_NOT_UPLOADED_FILE;
-import static io.restassured.RestAssured.given;
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+class FileEncryptionControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
 
-public class FileEncryptionControllerTest extends BaseRestTest {
-    public final String ENCRYPTION_URL = Urls.ENCRYPT_URL;
-    @AfterEach
-    public void deleteFile() {
-        deleteFile(TestValues.OUTPUT_TXT);
+    @Test
+    public void encryptAndExpectOkStatusCode() throws Exception {
+        MockMultipartFile file = new MockMultipartFile("file", "filename.txt", "text/plain", "(123 + 456) / 0".getBytes());
+
+        mockMvc.perform(MockMvcRequestBuilders.multipart("/api/v1/encrypt")
+                        .file(file)
+                        .queryParam("outputfile", "output.txt")
+                        .queryParam("key", "1234123412341234"))
+                .andExpect(status().is(200));
     }
 
     @Test
-    public void encryptFile() {
-        String fileName = TestValues.OUTPUT_TXT;
-        given()
-                .multiPart("file", new File(TestValues.INPUT_FILE_TXT))
-                .queryParam("outputfile", fileName)
-                .queryParam("key", TestValues.FIRST_KEY)
-                .when().post(ENCRYPTION_URL)
-                .then().statusCode(200);
+    public void calculateNotValidKeysAndExpectBadRequestStatusCode() throws Exception {
+        MockMultipartFile file = new MockMultipartFile("file", "filename.txt", "text/plain", "(123 + 456) / 0".getBytes());
 
-        boolean isFileExist = verifyFileIsUploaded(fileName);
-
-        Assertions.assertTrue(isFileExist, MESSAGE_FOR_NOT_UPLOADED_FILE(fileName));
-    }
-
-    @Test
-    public void encryptFileWithNotValidKeysReturnsBadRequestStatusCode() {
-        String fileName = TestValues.OUTPUT_TXT;
-        given()
-                .multiPart("file", new File(TestValues.INPUT_FILE_TXT))
-                .queryParam("outputfile", fileName)
-                .queryParam("key", "hello")
-                .when().post(ENCRYPTION_URL)
-                .then().statusCode(400);
+        mockMvc.perform(MockMvcRequestBuilders.multipart("/api/v1/calculate")
+                        .file(file)
+                        .queryParam("outputfile", "output.txt")
+                        .queryParam("key", "123"))
+                .andExpect(status().is(400));
     }
 }
