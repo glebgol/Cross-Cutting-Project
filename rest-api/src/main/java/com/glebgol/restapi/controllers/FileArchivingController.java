572daab0fc094e3500221adb8581d9698798a453
Gleb Golovnev <glebgolovnev2004@gmail.com>
2023-01-31 19:49:33 +0300

    Rest controller

--- rest-api/src/main/java/com/glebgol/restapi/controllers/FileArchivingController.java
+++ rest-api/src/main/java/com/glebgol/restapi/controllers/FileArchivingController.java
@@ -10,11 +10,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
 
+@RestController
 @RequestMapping("api/v1/")
 public class FileArchivingController {
     protected final String uploadPath = Constants.FILE_UPLOAD_PATH;
