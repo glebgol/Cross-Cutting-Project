aafa31d739be431733cee1bfb4e66e304b090ccb
Gleb Golovnev <glebgolovnev2004@gmail.com>
2023-02-23 21:47:50 +0300

    Add EncryptionService

--- rest-api/src/main/java/com/glebgol/restapi/controllers/FileEncryptionController.java
+++ rest-api/src/main/java/com/glebgol/restapi/controllers/FileEncryptionController.java
@@ -1,73 +1,69 @@
 package com.glebgol.restapi.controllers;
 
-import com.glebgol.businesslogic.utils.ciphers.CryptoUtils;
 import com.glebgol.businesslogic.utils.ciphers.KeyValidation;
+import com.glebgol.restapi.dto.EncryptionParamsDTO;
 import com.glebgol.restapi.responses.FileUploadResponse;
-import com.glebgol.restapi.utils.constants.Constants;
+import com.glebgol.restapi.services.EncryptionService;
 import com.glebgol.restapi.utils.FileDeleteUtil;
 import com.glebgol.restapi.utils.FileUploadUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
 
+import static com.glebgol.restapi.utils.constants.Constants.DOWNLOAD_URI;
+import static com.glebgol.restapi.utils.constants.Constants.FILE_UPLOAD_PATH;
+
 @RestController
 @RequestMapping("api/v1/")
+@RequiredArgsConstructor
 public class FileEncryptionController {
-    protected final String uploadPath = Constants.FILE_UPLOAD_PATH;
-    protected final String downloadUri = Constants.DOWNLOAD_URI;
+    private final EncryptionService encryptionService;
     @PostMapping("/encrypt")
-    public ResponseEntity<FileUploadResponse> encrypt(@RequestParam(value= "file") MultipartFile inputFile,
-                                                      @RequestParam(value = "outputfile") String outputFilename,
-                                                      @RequestParam(value="key") String key) throws IOException {
-        if (!KeyValidation.isValidDecryptionKey(key)) {
+    public ResponseEntity<FileUploadResponse> encrypt(EncryptionParamsDTO encryptionParamsDTO) {
+        if (!KeyValidation.isValidDecryptionKey(encryptionParamsDTO.getKey())) {
             return ResponseEntity.badRequest().build();
         }
-        FileUploadUtil.saveFile(uploadPath, inputFile);
+        FileUploadUtil.saveFile(FILE_UPLOAD_PATH, encryptionParamsDTO.getInputFile());
         File file = null;
         try {
-            file = new File(uploadPath + outputFilename);
-            CryptoUtils.encrypt(key, uploadPath + inputFile.getOriginalFilename(), file.getAbsolutePath());
+            file = encryptionService.encrypt(encryptionParamsDTO);
         } catch (Exception ex) {
             return ResponseEntity.internalServerError().build();
         } finally {
-            FileDeleteUtil.deleteFile(uploadPath, inputFile.getOriginalFilename());
+            FileDeleteUtil.deleteFile(FILE_UPLOAD_PATH, encryptionParamsDTO.getInputFile().getOriginalFilename());
         }
         FileUploadResponse response = FileUploadResponse.builder()
-                .fileName(outputFilename)
+                .fileName(encryptionParamsDTO.getOutputFilename())
                 .size(file.getTotalSpace())
-                .downloadUri(downloadUri + outputFilename)
+                .downloadUri(DOWNLOAD_URI + encryptionParamsDTO.getOutputFilename())
                 .build();
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @PostMapping("/decrypt")
-    public ResponseEntity<FileUploadResponse> decrypt(@RequestParam(value= "file") MultipartFile inputFile,
-                                                      @RequestParam(value = "outputfile") String outputFilename,
-                                                      @RequestParam(value="key") String key) throws IOException {
-        if (!KeyValidation.isValidDecryptionKey(key)) {
+    public ResponseEntity<FileUploadResponse> decrypt(EncryptionParamsDTO encryptionParamsDTO) {
+        if (!KeyValidation.isValidDecryptionKey(encryptionParamsDTO.getKey())) {
             return ResponseEntity.badRequest().build();
         }
-        FileUploadUtil.saveFile(uploadPath, inputFile);
+        FileUploadUtil.saveFile(FILE_UPLOAD_PATH, encryptionParamsDTO.getInputFile());
         File file = null;
         try {
-            file = new File(uploadPath + outputFilename);
-            CryptoUtils.decrypt(key, uploadPath + inputFile.getOriginalFilename(), file.getAbsolutePath());
+            file = encryptionService.decrypt(encryptionParamsDTO);
         } catch (Exception ex) {
             return ResponseEntity.internalServerError().build();
         } finally {
-            FileDeleteUtil.deleteFile(uploadPath, inputFile.getOriginalFilename());
+            FileDeleteUtil.deleteFile(FILE_UPLOAD_PATH, encryptionParamsDTO.getInputFile().getOriginalFilename());
         }
         FileUploadResponse response = FileUploadResponse.builder()
-                .fileName(outputFilename)
+                .fileName(encryptionParamsDTO.getOutputFilename())
                 .size(file.getTotalSpace())
-                .downloadUri(downloadUri + outputFilename)
+                .downloadUri(DOWNLOAD_URI + encryptionParamsDTO.getOutputFilename())
                 .build();
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
