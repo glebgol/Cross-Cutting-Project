314cf8e5ab5b9aa08e1b72ea4c94889d750a3ac9
Gleb Golovnev <glebgolovnev2004@gmail.com>
2023-02-24 18:57:18 +0300

    Add validation for file calculation

--- rest-api/src/main/java/com/glebgol/restapi/controllers/FileCalculationController.java
+++ rest-api/src/main/java/com/glebgol/restapi/controllers/FileCalculationController.java
@@ -1,17 +1,17 @@
 package com.glebgol.restapi.controllers;
 
-import com.glebgol.businesslogic.utils.ciphers.KeyValidation;
 import com.glebgol.restapi.dto.CalculationParamsDTO;
-import com.glebgol.restapi.responses.FileUploadResponse;
+import com.glebgol.restapi.dto.FileUploadResponse;
 import com.glebgol.restapi.services.CalculationService;
 import com.glebgol.restapi.utils.FileDeleteUtil;
 import com.glebgol.restapi.utils.FileUploadUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.io.File;
 import static com.glebgol.restapi.utils.constants.Constants.DOWNLOAD_URI;
 import static com.glebgol.restapi.utils.constants.Constants.FILE_UPLOAD_PATH;
@@ -21,20 +21,20 @@ import static com.glebgol.restapi.utils.constants.Constants.FILE_UPLOAD_PATH;
 @RequiredArgsConstructor
 public class FileCalculationController {
     private final CalculationService calculationService;
+
     @PostMapping("/calculate")
-    public ResponseEntity<FileUploadResponse> calculate(@Validated CalculationParamsDTO calculationParams) {
-        if (!KeyValidation.isValidForCalculation(calculationParams.getDecryptionKeys())) {
-            return ResponseEntity.badRequest().build();
+    public ResponseEntity<?> calculate(@Valid CalculationParamsDTO calculationParams, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity.badRequest().body(bindingResult.getFieldError().getDefaultMessage());
         }
         FileUploadUtil.saveFile(FILE_UPLOAD_PATH, calculationParams.getInputFile());
-
         File file = null;
         try {
             file = calculationService.calculate(calculationParams);
         } catch (Exception ex) {
             return ResponseEntity.internalServerError().build();
         } finally {
-            FileDeleteUtil.deleteFile(DOWNLOAD_URI, calculationParams.getInputFile().getOriginalFilename());
+            FileDeleteUtil.deleteFile(FILE_UPLOAD_PATH, calculationParams.getInputFile().getOriginalFilename());
         }
         FileUploadResponse response = FileUploadResponse.builder()
                 .fileName(calculationParams.getOutputFilename())
--- rest-api/src/main/java/com/glebgol/restapi/dto/CalculationParamsDTO.java
+++ rest-api/src/main/java/com/glebgol/restapi/dto/CalculationParamsDTO.java
@@ -1,23 +1,27 @@
 package com.glebgol.restapi.dto;
 
-import com.sun.istack.NotNull;
+import com.glebgol.restapi.validation.DecryptionKeysForCalculation;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public final class CalculationParamsDTO {
-    @NotNull
+    @NotNull(message = "input file can't be null")
     private MultipartFile inputFile;
-    @NotNull
+    @NotBlank(message = "output file name can't be null or empty")
     private String outputFilename;
     private Boolean isZipped;
+    @DecryptionKeysForCalculation(message = "fuck")
     private List<String> decryptionKeys;
-    @NotNull
+    @Pattern(regexp = "txt|xml|json", message = "Calculation supports only txt|xml|json extensions")
     private String extension;
 }
